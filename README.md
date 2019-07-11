d5786ed0d30e591081f5019ea3b363bf2fd3f728
actiqx <priyatoshpatra@gmail.com>
2019-07-11 16:20:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-2liy7w-wmac7p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-2liy7w-wmac7p)
---
+++ app/paginator-configurable-example.ts
@@ -0,0 +1,61 @@
+import {Component} from '@angular/core';
+import {PageEvent} from '@angular/material';
+
+/**
+ * @title Configurable paginator
+ */
+@Component({
+  selector: 'paginator-configurable-example',
+  templateUrl: 'paginator-configurable-example.html',
+  styleUrls: ['paginator-configurable-example.css'],
+})
+
+export class PaginatorConfigurableExample {
+
+  // MatPaginator Inputs
+  length = 1000;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+selectedChips=[];
+  // MatPaginator Output
+  pageEvent: PageEvent;
+
+  datasource = [];
+
+  activePageDataChunk = []
+
+  constructor() {
+    // Generaing dummy content
+    for (let i = 0; i < 1000; i++) {
+      let dummyObject = { field: `abc${i+1}`,state:false} 
+      this.datasource.push(dummyObject);
+    }
+    this.activePageDataChunk = this.datasource.slice(0,this.pageSize);
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+  }
+
+  onPageChanged(e) {
+    let firstCut = e.pageIndex * e.pageSize;
+    let secondCut = firstCut + e.pageSize;
+    this.activePageDataChunk = this.datasource.slice(firstCut, secondCut);
+  }
+  changeSelected(parameter: string, query: string) {
+
+let index = this.selectedChips.indexOf(query);
+if (index >= 0) {
+    this.selectedChips.splice(index, 1);
+} else {
+    this.selectedChips.push(query);
+}
+console.log("this. selectedChips " + this.selectedChips  );
+
+}
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
